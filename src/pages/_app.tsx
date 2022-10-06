41c82ba448009d62e2de90f4eae076c1d2a1b483
Kaustav Ghosh <teetangh@gmail.com>
2022-10-06 15:57:48 +0530

    updated signup and login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect } from "react";
-import Head from "next/head";
-import { ThemeProvider } from "@mui/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/styles";
+import Head from "next/head";
+import React, { useEffect } from "react";
 // import Header from "../components/Header";
-import theme from "../theme";
+import Amplify from "aws-amplify";
 import type { AppProps } from "next/app";
-// import Amplify from "aws-amplify";
+import theme from "../theme";
 
-// import awsconfig from "../aws-exports";
-// import AuthContext from "../context/AuthContext";
+import awsconfig from "../aws-exports";
+import AuthContext from "../context/AuthContext";
 
-// Amplify.configure({ ...awsconfig, ssr: true });
+Amplify.configure(awsconfig);
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -24,20 +24,20 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <React.Fragment>
       <Head>
-        <title>Reddit Clone</title>
+        <title>Social Media App</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
-      {/* <AuthContext> */}
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        {/* <Header /> */}
-        <Component {...pageProps} />
-      </ThemeProvider>
-      {/* </AuthContext> */}
+      <AuthContext>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          {/* <Header /> */}
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </AuthContext>
     </React.Fragment>
   );
 }
